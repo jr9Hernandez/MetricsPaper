7988fca64991654f7e4825ea0a1e065400247072
Julian <richardes71@gmail.com>
2015-12-09 16:49:13 -0200

    working

--- PaperMetrics/src/dk/itu/mario/scene/LevelSceneTest.java
+++ PaperMetrics/src/dk/itu/mario/scene/LevelSceneTest.java
@@ -80,7 +80,7 @@ public class LevelSceneTest extends LevelScene {
 				
 				
 				//test gral
-				ReadXsl objRead=new ReadXsl();
+				ReadXsl objRead=new ReadXsl(); 
 				objRead.reading(320, 15, levelSeed, levelDifficulty, levelType);
 				//objRead.compresionDistance(320, 15, levelSeed, levelDifficulty, levelType);
 			    currentLevel = LevelGenerator.createLevel(320, 15, levelSeed, levelDifficulty, levelType);
